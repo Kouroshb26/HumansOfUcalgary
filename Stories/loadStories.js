e85fbbe1f6db66d93cff7a4582737debe4968b55
Kourosh Banaeianzadeh <kourosh@synopsys.com>
2018-09-29 14:30:10 -0600

    Update stories from Instagram

--- Stories/loadStories.js
+++ Stories/loadStories.js
@@ -1,17 +1,28 @@
 $( document ).ready(function() {
 
-    $.getJSON("stories.json",function(data){
-        for (let i of data){
-            console.log(i)
-            var card = $("<div>").addClass("col-lg-6 portfolio-item story")
-                .append($("<div>").addClass("card h-100")
-                    .append($("<img>").addClass("card-img-top img-fluid").css("background-image", "url("+i.image+")").height("500px"))
-                    .append($("<div>").addClass("card-body")
-                        .append($("<p>").css("white-space","pre-line").addClass("card-text").text(i.text))))
 
-            $("#content").append(card)
+    $(".bricklayer").bricklayer()
+
+
+
+    $.getJSON("media.json",function(data){
+        for (let i of data.photos){
+            console.log(i)
+
+            var card = $("<div>").addClass("portfolio-item story")
+
+            card
+                .append($("<div>").addClass("card")
+                    .append($("<img>").addClass("card-img-top").attr("src",i.path))
+                    .append($("<div>").addClass("card-body")
+                        .append($("<p>").css("white-space","pre-line").addClass("card-text").text(i.caption))
+                    )
+                )
+
+            $(".bricklayer").appendElements(card)
 
         }
     })
+
 
 });
--- spotlight.js
+++ spotlight.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
 
-    $.getJSON("Stories/stories.json",function(data){
-        let i = data[0]
+    $.getJSON("Stories/media.json",function(data){
+        let i = data.photos[0]
 
-        $("#spotlightText").text(i.text).css("white-space","pre-line")
-        $("#spotlightImage").attr("src","Stories/"+i.image)
+        $("#spotlightText").text(i.caption).css("white-space","pre-line")
+        $("#spotlightImage").attr("src","Stories/"+i.path)
 
     })
 
