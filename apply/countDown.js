6031c5a2adecebe567643d40f02b630ed4205d66
Kourosh Banaeianzadeh <kourosh@synopsys.com>
2019-09-02 11:21:00 -0700

    Add help text if form does not load

--- apply/countDown.js
+++ apply/countDown.js
@@ -8,7 +8,7 @@ var timer = setInterval(function() {
     var now = new Date().getTime();
 
     if(now > applicationStart && now < applicationEnd ) {
-        document.getElementById("PhotoJournalistForm").innerHTML = "<iframe  src=\"https://docs.google.com/forms/d/e/1FAIpQLSc2N5g0RPqsSOleS7enxNhMNMtAcgeAk7hxavxFgtw-RsJE_g/viewform?embedded=true\" width=\"100%\" height=\"1000\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\">Loading...</iframe>";
+        document.getElementById("PhotoJournalistForm").innerHTML = "<p> If you are not seeing the form please click <a href='https://docs.google.com/forms/d/e/1FAIpQLSc2N5g0RPqsSOleS7enxNhMNMtAcgeAk7hxavxFgtw-RsJE_g/viewform'>here</a></p> <iframe  src=\"https://docs.google.com/forms/d/e/1FAIpQLSc2N5g0RPqsSOleS7enxNhMNMtAcgeAk7hxavxFgtw-RsJE_g/viewform?embedded=true\" width=\"100%\" height=\"1000\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\">Loading...</iframe>";
         clearInterval(timer);
     }
 
